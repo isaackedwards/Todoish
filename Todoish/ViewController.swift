583a0915acbdeb767b2c2a2b5fe26e1816ff0915
Edward <edward.adobedesigns@gmail.com>
2020-12-14 15:40:03 +0300

    Initial Commit

---
+++ Todoish/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoish
+//
+//  Created by Edward Isaack on 12/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
